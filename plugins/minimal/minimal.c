49c50c86431f3d113e749b0ac4d5b5156cff4e24
Niklas Gray <niklas@ourmachinery.com>
2020-03-05 08:44:49 -0800

    Minimal plugin sample

---
+++ plugins/minimal/minimal.c
@@ -0,0 +1,12 @@
+static struct tm_logger_api* tm_logger_api;
+
+#include <foundation/api_registry.h>
+
+#include <foundation/log.h>
+
+TM_DLL_EXPORT void load_minimal(struct tm_api_registry_api* reg, bool load)
+{
+    tm_logger_api = reg->get(TM_LOGGER_API_NAME);
+
+    TM_LOG("Minimal plugin %s.\n", load ? "loaded" : "unloaded");
+}
