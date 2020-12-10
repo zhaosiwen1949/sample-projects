d7d73238df0d64b33c46b631e5eebaf0d326321e
Karl Zylinski <karl@ourmachinery.com>
2020-12-10 15:06:21 +0100

    Updated sample code to be in line with Link and Transform component merge.

--- plugins/custom_component/custom_component.c
+++ plugins/custom_component/custom_component.c
@@ -1,11 +1,10 @@
 static struct tm_entity_api* tm_entity_api;
-static struct tm_link_component_api* tm_link_component_api;
+static struct tm_transform_component_api* tm_transform_component_api;
 static struct tm_temp_allocator_api* tm_temp_allocator_api;
 static struct tm_the_truth_api* tm_the_truth_api;
 static struct tm_localizer_api* tm_localizer_api;
 
 #include <plugins/entity/entity.h>
-#include <plugins/entity/link_component.h>
 #include <plugins/entity/transform_component.h>
 #include <plugins/the_machinery_shared/component_interfaces/editor_ui_interface.h>
 
@@ -78,12 +77,9 @@ static void engine_update__custom(tm_engine_o* inst, tm_engine_update_set_t* dat
 {
     TM_INIT_TEMP_ALLOCATOR(ta);
 
-    tm_entity_t* mod_link = 0;
     tm_entity_t* mod_transform = 0;
 
     struct tm_entity_context_o* ctx = (struct tm_entity_context_o*)inst;
-    const uint32_t link_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__LINK_COMPONENT);
-    void* link_manager = tm_entity_api->component(ctx, link_component)->manager;
 
     double t = 0;
     for (const tm_entity_blackboard_value_t* bb = data->blackboard_start; bb != data->blackboard_end; ++bb) {
@@ -94,26 +90,17 @@ static void engine_update__custom(tm_engine_o* inst, tm_engine_update_set_t* dat
     for (tm_engine_update_array_t* a = data->arrays; a < data->arrays + data->num_arrays; ++a) {
         struct tm_custom_component_t* custom = a->components[0];
         tm_transform_component_t* transform = a->components[1];
-        tm_link_component_t* link = a->components[2];
 
         for (uint32_t i = 0; i < a->n; ++i) {
             if (!custom[i].y0)
-                custom[i].y0 = transform[i].tm.pos.y;
+                custom[i].y0 = transform[i].world.pos.y;
             const float y = custom[i].y0 + custom[i].amplitude * sinf((float)t * custom[i].frequency);
 
-            if (link) {
-                link[i].local_transform.pos.y = y;
-                tm_carray_temp_push(mod_link, a->entities[i], ta);
-            } else {
-                transform[i].tm.pos.y = y;
-                ++transform[i].version;
-                tm_carray_temp_push(mod_transform, a->entities[i], ta);
-            }
+            transform[i].world.pos.y = y;
+            ++transform[i].version;
+            tm_carray_temp_push(mod_transform, a->entities[i], ta);
         }
     }
-
-    for (tm_entity_t* e = mod_link; e != tm_carray_end(mod_link); ++e)
-        tm_link_component_api->transform(link_manager, *e);
 
     tm_entity_api->notify(ctx, data->engine->components[1], mod_transform, (uint32_t)tm_carray_size(mod_transform));
 
@@ -129,13 +116,12 @@ static void component__register_engine(struct tm_entity_context_o* ctx)
 {
     const uint32_t custom_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__CUSTOM_COMPONENT);
     const uint32_t transform_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__TRANSFORM_COMPONENT);
-    const uint32_t link_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__LINK_COMPONENT);
 
     const tm_engine_i custom_engine = {
         .name = "Custom Component",
-        .num_components = 3,
-        .components = { custom_component, transform_component, link_component },
-        .writes = { false, true, true },
+        .num_components = 2,
+        .components = { custom_component, transform_component },
+        .writes = { false, true },
         .update = engine_update__custom,
         .filter = engine_filter__custom,
         .inst = (tm_engine_o*)ctx,
@@ -146,7 +132,7 @@ static void component__register_engine(struct tm_entity_context_o* ctx)
 TM_DLL_EXPORT void tm_load_plugin(struct tm_api_registry_api* reg, bool load)
 {
     tm_entity_api = reg->get(TM_ENTITY_API_NAME);
-    tm_link_component_api = reg->get(TM_LINK_COMPONENT_API_NAME);
+    tm_transform_component_api = reg->get(TM_TRANSFORM_COMPONENT_API_NAME);
     tm_the_truth_api = reg->get(TM_THE_TRUTH_API_NAME);
     tm_temp_allocator_api = reg->get(TM_TEMP_ALLOCATOR_API_NAME);
     tm_localizer_api = reg->get(TM_LOCALIZER_API_NAME);
