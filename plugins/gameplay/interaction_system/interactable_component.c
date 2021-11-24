ffda1676c7ed1d84daef45fabf025f55050994a2
Karl Zylinski <karl.zylinski@ourmachinery.com>
2021-11-24 14:41:25 +0100

    Fixes for changes in properties view API.

--- plugins/gameplay/interaction_system/interactable_component.c
+++ plugins/gameplay/interaction_system/interactable_component.c
@@ -425,10 +425,7 @@ static float component_properties_ui(struct tm_properties_ui_args_t* args, tm_re
         TM_LOCALIZE("Rotating Door"),
     };
 
-    const tm_rect_t label_r = tm_rect_split_left(item_rect, args->label_width, args->metrics[TM_PROPERTIES_METRIC_MARGIN], 0);
-    const tm_rect_t dropdown_r = tm_rect_split_left(item_rect, args->label_width, args->metrics[TM_PROPERTIES_METRIC_MARGIN], 1);
-
-    tm_properties_view_api->ui_label(args, label_r, TM_LOCALIZE("Type"), 0);
+    const tm_rect_t dropdown_r = tm_properties_view_api->ui_label_split(args, label_r, TM_LOCALIZE("Type"), 0);
 
     const tm_ui_dropdown_t d = {
         .rect = dropdown_r,
